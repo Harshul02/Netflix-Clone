ace0e9767507f40e6f6026ad6d8b6225b6f0e0b2
Harshul Jain <harshulgangwal02@gmail.com>
2023-05-27 15:48:13 +0530

    Navbar

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Main = () => {
+
+    const [movies, setMovies] = useState([]);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Main
