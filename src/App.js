c7441c80428ec127db66e13542babe20c9ac30b1
Harshul Jain <harshulgangwal02@gmail.com>
2023-05-29 19:45:02 +0530

    Added Signup Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
     <Navbar />
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/account' element={<Account />} />
     </Routes>
     </AuthContextProvider>
     </>
